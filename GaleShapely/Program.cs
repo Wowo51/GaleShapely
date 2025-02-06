95ce217541614cd9f11c8f2bcaaffef804bc3f55
Warren Harding <warrencharding@yahoo.com>
2025-02-06 13:44:11 -0700

    Add GaleShapely/Program.cs

---
+++ GaleShapely/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GaleShapely
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+        // Minimal entry point. No test code.
+        }
+    }
+}
